991aae1023c3dab163fa577629cea747755f8b79
Lionel Ringenbach <ucodia@live.com>
2025-07-22 17:15:30 -0700

    Integration of prototype UI

--- src/extension/extension/vscode-node/contributions.ts
+++ src/extension/extension/vscode-node/contributions.ts
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { WattsupDashboard } from '../../../wattsup/wattsupDashboard';
 import { AuthenticationContrib } from '../../authentication/vscode-node/authentication.contribution';
 import { BYOKContrib } from '../../byok/vscode-node/byokContribution';
 import { ChatQuotaContribution } from '../../chat/vscode-node/chatQuota.contribution';
@@ -112,5 +113,6 @@ export const vscodeNodeChatContributions: IExtensionContributionFactory[] = [
 	asContributionFactory(BYOKContrib),
 	asContributionFactory(McpSetupCommands),
 	asContributionFactory(LanguageModelProxyContrib),
+	asContributionFactory(WattsupDashboard),
 	newWorkspaceContribution,
 ];
